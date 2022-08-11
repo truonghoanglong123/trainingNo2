585c2c580304ceb2198d6e465a338415e89a05b2
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2022-08-11 22:16:17 +0700

    first-dev

---
+++ README.md
@@ -0,0 +1,4 @@
+# trainingNo2
+tham khỏa
+https://codepen.io/lanmi/pen/NZEpog
+https://codepen.io/naafi/pen/qBbYEXe
---
+++ js/app.js
@@ -0,0 +1,11 @@
+function swapImages(){
+    var $active = $('#myGallery .active');
+    var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
+    $active.fadeOut(function(){
+        $active.removeClass('active');
+        $next.fadeIn().addClass('active');
+    });
+}
+  
+  // Run our swapImages() function every 5secs
+ setInterval('swapImages()', 1000);
