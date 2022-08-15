9df9aa0fc8033e9b87dc05f752d577f378ca68de
unknown <truonghoanglongxyz@gmail.com>
2022-08-15 17:29:10 +0700

    thu2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-function swapImages(){
-    var $active = $('#myGallery .active');
-    var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
-    $active.fadeOut(function(){
-        $active.removeClass('active');
-        $next.fadeIn().addClass('active');
-    });
-}
-setInterval('swapImages()', 3000);
+// function swapImages(){
+//     var $active = $('#myGallery .active');
+//     var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
+//     $active.fadeOut(function(){
+//         $active.removeClass('active');
+//         $next.fadeIn(2000).addClass('active');
+//     });
+// }
+// setInterval('swapImages()', 2000);
+
+// function swapImages2(){
+//     var $active = $('#recruit .active');
+//     var $next = ($('#recruit .active').next().length > 0) ? $('#recruit .active').next() : $('#recruit img:first');
+//     $active.fadeOut(function(){
+//         $active.removeClass('active');
+//         $next.fadeIn(2000).addClass('active');
+//     });
+// }
+// setInterval('swapImages2()', 2000);
