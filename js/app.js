6da3c16cf80b102130d02b51baca83b072c99b66
unknown <truonghoanglongxyz@gmail.com>
2022-08-16 16:46:39 +0700

    4h45

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//imghideshow
 // function swapImages(){
 //     var $active = $('#myGallery .active');
 //     var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
@@ -7,13 +8,3 @@
 //     });
 // }
 // setInterval('swapImages()', 2000);
-
-// function swapImages2(){
-//     var $active = $('#recruit .active');
-//     var $next = ($('#recruit .active').next().length > 0) ? $('#recruit .active').next() : $('#recruit img:first');
-//     $active.fadeOut(function(){
-//         $active.removeClass('active');
-//         $next.fadeIn(2000).addClass('active');
-//     });
-// }
-// setInterval('swapImages2()', 2000);
---
+++ js/backToTop.js
@@ -0,0 +1,41 @@
+//pagebacktop
+var rootElement = document.documentElement
+var backtop = document.querySelector('.backtop')
+
+function handleScroll(){
+    if(window.pageYOffset > 500){
+        backtop.classList.remove('btnshow');
+    }
+    else{
+        backtop.classList.add('btnshow');
+    }
+}
+
+function scrollToTop(){
+    rootElement.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+}
+
+backtop.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
+
+
+
+//fixmenu
+var menu = document.querySelector('.section-menu')
+var section = document.querySelector('.section-service')
+function handleScrollMenu(){
+    // console.log(window.pageYOffset);
+    if(window.pageYOffset > 950){
+        menu.classList.add('fixmenu')
+        section.classList.add('gat')
+    }
+    else{
+        menu.classList.remove('fixmenu')
+        section.classList.remove('gat')
+    }
+}
+
+document.addEventListener("scroll", handleScrollMenu)
